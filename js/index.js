407897d55a429f63a8cf83ce1a3884060155cece
J-Place <jay@cooper-james.comjplace@usmastersswimming.org>
2020-02-07 02:06:02 -0500

    markup updates, filter toggle js, border changed to top of list item

--- js/index.js
+++ js/index.js
@@ -1,45 +1,45 @@
-function getWorkoutData() {
-    var xhr = new XMLHttpRequest();
-    // xhr.open('GET', '/swimFeedJP.json?ver=1', true);
-    // xhr.open('GET', '/KD Great Workout.json', true);
-    xhr.open('GET', 'https://j-place.github.io/KD%20Great%20Workout.json', true);
-    xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.response);
-            var workoutData = response;
-            renderWorkouts(workoutData);
-            console.log(workoutData);
-            return;
-        }
-        console.log("Error retrieving swim information");
-        return null;
-    };
-    xhr.send();
-}
-// getWorkoutData();
+// function getWorkoutData() {
+//     var xhr = new XMLHttpRequest();
+//     // xhr.open('GET', '/swimFeedJP.json?ver=1', true);
+//     // xhr.open('GET', '/KD Great Workout.json', true);
+//     xhr.open('GET', 'https://j-place.github.io/KD%20Great%20Workout.json', true);
+//     xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+//     xhr.onload = function () {
+//         if (xhr.status === 200) {
+//             var response = JSON.parse(xhr.response);
+//             var workoutData = response;
+//             renderWorkouts(workoutData);
+//             console.log(workoutData);
+//             return;
+//         }
+//         console.log("Error retrieving swim information");
+//         return null;
+//     };
+//     xhr.send();
+// }
+// // getWorkoutData();
 
 
- console.log("TEST");
+//  console.log("TEST");
 
 
- fetch("https://j-place.github.io/KD%20Great%20Workout.json").then(function(response){
-        return response.json();
-    }).then(function(data){
-        // console.log(data);
-        renderWorkouts(data);
-    });
+//  fetch("https://j-place.github.io/KD%20Great%20Workout.json").then(function(response){
+//         return response.json();
+//     }).then(function(data){
+//         // console.log(data);
+//         renderWorkouts(data);
+//     });
 
 // Populate Workout Results Template
-function renderWorkouts(workout) {
-    console.log(workout.workouttitle);
-    console.log(workout)
-    var rawTemplate = document.getElementById("workoutResultsTemplate").innerHTML;
-    var compiledTemplate = Handlebars.compile(rawTemplate);
-    var ourGeneratedHTML = compiledTemplate(workout)
-    var wrapper = document.getElementById("workoutResultsWrapper");
-    wrapper.innerHTML = ourGeneratedHTML;
-}
+// function renderWorkouts(workout) {
+//     console.log(workout.workouttitle);
+//     console.log(workout)
+//     var rawTemplate = document.getElementById("workoutResultsTemplate").innerHTML;
+//     var compiledTemplate = Handlebars.compile(rawTemplate);
+//     var ourGeneratedHTML = compiledTemplate(workout)
+//     var wrapper = document.getElementById("workoutResultsWrapper");
+//     wrapper.innerHTML = ourGeneratedHTML;
+// }
 
 
 
@@ -57,3 +57,19 @@ function renderWorkouts(workout) {
 $('#printWorkout').click(function () {
     window.print();
 });
+
+
+$(window).resize(function() {
+    var width = $(window).width();
+    if (width < 768){
+        $(".list-control-search__content").css('display', 'none');
+    } else {
+        $(".list-control-search__content").css('display', 'flex');
+    }
+  });
+
+
+$('.list-control-search-mobile__toggle').click(function() {
+    var active = $(this).toggleClass('show-search').hasClass('show-search');
+    $(".list-control-search__content").css('display', !active ? 'none' : 'flex');
+  });
